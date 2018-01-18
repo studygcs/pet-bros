6411089ace490a2742575dc954338c161e23bcb5
sebawita <sebawita@gmail.com>
2018-01-18 16:31:11 +0000

    Updating lazy loaded paths to be the same for both web and {N}

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,7 +20,7 @@ const authGuards = {
  * (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
  */
 export const AppRoutes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
   {
     path: 'login',
     children: loginRoutes,
@@ -30,27 +30,27 @@ export const AppRoutes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: 'app/home/home.module#HomeModule',
+    loadChildren: '~/app/home/home.module#HomeModule',
     ...authGuards
   },
   {
     path: 'petSearch',
-    loadChildren: 'app/pet-search/pet-search.module#PetSearchModule',
+    loadChildren: '~/app/pet-search/pet-search.module#PetSearchModule',
     ...authGuards
   },
   {
     path: 'shelterSearch',
-    loadChildren: 'app/shelter-search/shelter-search.module#ShelterSearchModule',
+    loadChildren: '~/app/shelter-search/shelter-search.module#ShelterSearchModule',
     ...authGuards
   },
   {
     path: 'pet/:petId',
-    loadChildren: 'app/pet-details/pet-details.module#PetDetailsModule',
+    loadChildren: '~/app/pet-details/pet-details.module#PetDetailsModule',
     ...authGuards
   },
   {
     path: 'shelter/:shelterId',
-    loadChildren: 'app/shelter-details/shelter-details.module#ShelterDetailsModule',
+    loadChildren: '~/app/shelter-details/shelter-details.module#ShelterDetailsModule',
     ...authGuards
   }
 ];
